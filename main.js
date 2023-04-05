016d428f99406ae43fe007b31212254392e97361
Daniel Karanja <sirkaranja12@gmail.com>
2023-04-05 18:11:06 +0300

    submit func

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ fetch("http://localhost:3000/tours")
     tourDiv.innerHTML = `
      
       <img src="${tour.tour_image}" id="tour-image " alt="${tour.title}">
+     <button  class="love-button"><i class="fa fa-heart"></i> </button>
       <p class="love-emoji"> ${tour.like}</p>
       <h3 id="tour-title ">${tour.title}</h3>
       <p id="tour-price"> ${tour.price}</p>
@@ -19,6 +20,34 @@ fetch("http://localhost:3000/tours")
     // append the tour div to the container-deal div
     document.querySelector('.container-deal').appendChild(tourDiv);
   });
+ 
+ //from form
+ const form = document.querySelector('#feedback');
+ form.addEventListener('submit',handleSubmit);
+
+ //define function to be called when form is submitted
+ function handleSubmit(e) {
+  e.preventDefault();
+  let feedbackObj={
+    name: e.target.name.value,
+    number: e.target.number.value,
+    email: e.target.email.value,
+    message: e.target.message.value,
+
+  }
+  console.log(feedbackObj)
+  addfeedback(feedbackObj);
+}
+ function addfeedback(feedbackObj) {
+fetch('http://localhost:3000/feedback', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify(feedbackObj)
+})
+.then(res =>res.json())
+.then(feedback =>console.log(feedback))
+.catch(err =>console.error(err));
+ }
 });
 
 
